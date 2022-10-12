555e0375825cc7455a897323862b114868d93f68
RCmags <memoryofatrufestival@gmail.com>
2022-10-12 16:16:38 -0500

    fixed incompatibility, improved examples

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # vector_datatype
 This is a library for 3d vectors and quaternions. It includes two structs to define each datatype. Both vectors and quaternions can perform basic operations such as addition, subtraction, multiplication, division, increment, decrement, and their analogs for multiplication and division. Vectors and quaternions are also compatible with each other.  
 
-Included are other operations such as vector dot product and cross product. Quaternions can also be constructed as unit quaternions to represent rotations. They can also rotate vectors. Both datatypes are compatible with arrays, meaning that a length-3 array can be added, subtracted, etc from a vector. This also applies to quaternions.  
+Other operations included such as vector dot product and cross product. Quaternions can also rotate and scale a vector. They can also be constructed as unit quaternions to represent a pure rotation. Both datatypes are compatible with arrays, meaning that a length-3 array can be added, subtracted, etc from a vector. This is the same for quaternions.  
 
 For more information on vector and quaternion operations, see these links:  
 
@@ -9,6 +9,6 @@ For more information on vector and quaternion operations, see these links:
 - [Quaternion operations](http://graphics.stanford.edu/courses/cs348a-17-winter/Papers/quaternion.pdf)  
 - [Vector operations](http://emweb.unl.edu/Math/mathweb/vectors/vectors.html)
 
-<img src = "https://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/transforms/axis.gif"></img>
-<img src = "https://opentextbc.ca/calculusv3openstax/wp-content/uploads/sites/288/2019/08/CNX_Calc_Figure_12_02_011.jpg"></img>
+![alt text](http://danceswithcode.net/engineeringnotes/quaternions/images/axis-angle.png)
+![alt text](https://opentextbc.ca/calculusv3openstax/wp-content/uploads/sites/288/2019/08/CNX_Calc_Figure_12_02_011.jpg)
 
--- src/quaternion_type.h
+++ src/quaternion_type.h
@@ -1,69 +1,75 @@
-#include "arduino.h"
+#include <Arduino.h>
 #include "vector_type.h"
 
 #ifndef quaternion_type_h
 #define quaternion_type_h
+
+// input labels
+#define LOCAL_FRAME		false
+#define GLOBAL_FRAME	true
+
+#define SMALL_ANGLE		true
+#define LARGE_ANGLE		false
  
 //------- 4D Quaternion --------
 
 struct quat_t {
-    // Components:
-    float  w;  
-    vec3_t v;
+    // Components
+    float  w;		// scalar
+    vec3_t v;		// vector
 
-    // Constructors:
+    // 0. Constructors:
     quat_t();
     quat_t( float, float, float, float );
     quat_t( float, vec3_t );
     quat_t( vec3_t );
     quat_t( float [] );
-
-    //- Basic Operations:
-
-    // Addition and subtraction:
+    
+    // 1. Basic Operations:
+    	// Addition and subtraction:
     quat_t operator + ( const quat_t & );
     quat_t operator - ( const quat_t & );
     quat_t operator - ( void );
     void operator += ( const quat_t & );
     void operator -= ( const quat_t & );
 
-    // Scalar product and division:
+    	// Scalar product and division:
     quat_t operator * ( const float );
     quat_t operator / ( const float );
     void operator *= ( const float );
     void operator /= ( const float );
 
-    //- Important operations:
-    quat_t conj();
-    float inner();
-    float mag();
-    quat_t norm();
-
-    //- Quaternion multiplication and division:
+    	// Quaternion multiplication and division:
     quat_t operator * ( const quat_t & );
     quat_t operator / ( quat_t & );
     void operator *= ( const quat_t & );
     void operator /= ( quat_t & );
 
-    //- Set as rotation transform:
+    // 3. Important operations:
+    quat_t conj();
+    quat_t norm();
+    float inner();
+    float mag();
 
-    // Axis and angle 
-    void setRotation( const bool, vec3_t, float );
-    void setRotation( const bool, vec3_t );
+    // 4. Rotation transform:
+    	// axis and angle 
+    void setRotation( vec3_t, float, const bool );
+    void setRotation( vec3_t, const bool );
 
-    // Vector rotation:
-    vec3_t rotate( const bool, vec3_t );
+    	// vector rotation
+    vec3_t rotate( vec3_t, const bool );
 
-    // Axis vector projections:
+    	// axis projections
     vec3_t axisX( const bool );
     vec3_t axisY( const bool );
     vec3_t axisZ( const bool );
 };
 
-// Vector multiplication - Result is quaternion
+// 1B. Global operators:
+	// vector multiplication
 quat_t operator * ( vec3_t &, vec3_t & );
 
-// Reverse order - Scalar product
+	// reverse order scalar product
 quat_t operator * ( const float, quat_t & );
 
 #endif
